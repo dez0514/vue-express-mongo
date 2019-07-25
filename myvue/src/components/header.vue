a71052d068c3113f5dc43bb7460bdefd120441d3
zhangwangde <2573433803@qq.com>
2019-07-25 17:38:13 +0800

    111

--- myvue/src/components/header.vue
+++ myvue/src/components/header.vue
@@ -1,21 +1,24 @@
 <template>
-    <div>
-        <nav class="navbar navbar-default" role="navigation">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-                    <span class="sr-only">展开导航</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">mytest</a>
+    <div class="header">
+        <img class="togit" src="./images/togit.png" alt="">
+        <div class="inner-header">
+            <div class="logo-wrap">
+                <div class="logo">wade</div>
+                <div class="openbtn" @click="changeShowList">
+                    <span style="margin-top:0"></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
-            <div class="collapse navbar-collapse" id="menu">
-                <ul class="nav navbar-nav">
-                    <li v-for="(item,index) in tablist" :key="index">{{item.txt}}</li>
-                </ul>
-            </div>
-        </nav>
+            <transition name="draw">
+                <div class="inner-list"  v-show="boxshow">
+                    <div class="inner-item" v-for="(item,index) in tablist" :key="index">
+                        <div><img class="item-icon" :src="item.icon" alt=""></div>
+                        <div class="item-txt">{{item.txt}}</div>
+                    </div>
+                </div>
+            </transition>
+        </div> 
     </div>
 </template>
 <script>
@@ -23,18 +26,136 @@ export default {
     data(){
         return {
             tablist: [
-                {txt:'首页'},
-                {txt:'分类'},
-                {txt:'标签'},
-                {txt:'归档'}
-            ]
+                {txt:'首页',icon: require('./images/ico-home.png')},
+                {txt:'分类',icon: require('./images/ico-group.png')},
+                {txt:'标签',icon: require('./images/ico-tags.png')},
+                {txt:'归档',icon: require('./images/ico-record.png')},
+                {txt:'关于',icon: require('./images/ico-mine.png')},
+                {txt:'搜索',icon: require('./images/ico-search.png')}
+            ],
+            showlist: false,
+            boxshow:false
+        }
+    },
+    methods: {
+        changeShowList(){
+            // this.showlist = !this.showlist
+            this.boxshow = !this.boxshow
         }
     }
 }
 </script>
 
-<style>
- .navbar {
-     width: 100%;
+<style lang="scss" scoped>
+  .inner-list {
+    overflow: hidden;
+    height: 182px;
+    font-size: 13px;
+    color: #34495e;
+    border-top: 1px solid #ddd;
+    .inner-item:nth-of-type(1){
+        margin-top: 10px;
+    }
+  }
+ .header {
+    position: relative;
+    background: #f5f5f5;
+  }
+  .inner-header {
+        position: relative;
+        padding: 10px 10px 0 10px;
+        margin: 0 auto;
+        .logo-wrap {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            padding-bottom: 10px;
+        }
+        .logo {
+            margin-left: 10px;
+            font-size: 22px;
+            font-weight: bolder;
+            cursor: pointer;
+        }
+        .openbtn {
+            padding: 9px 10px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            cursor: pointer;
+            span {
+                margin-top: 4px;
+                display: block;
+                width: 22px;
+                height: 2px;
+                background: #555;
+                border-radius: 1px;
+            }
+        }
+        
+        .inner-item {
+            display: flex;
+            align-items: center;
+            padding: 5px 10px;
+            border-radius: 2px;
+            cursor: pointer;
+            .item-icon {
+                display: block;
+                width: 13px;
+            }
+            .item-txt {
+                margin-left: 5px;
+            }
+        }
+        .inner-item:hover,.openbtn:hover {
+            background: #E1E1E1;
+        }
+    }
+  .togit {
+     display: none;
+  }
+  .draw-enter-active, .draw-leave-active {
+    transition: all 1s ease;
+   }
+   .draw-enter,
+  .draw-leave-to {
+    height: 0;
+  }
+ @media (min-width: 768px) {
+    .inner-header {
+        padding: 25px 0;
+        width: 700px;
+        display: flex;
+        flex-direction: row-reverse;
+        .logo {
+            position: absolute;
+            left: -10px;
+            top: 50%;
+            transform: translate(-50%,-50%);
+        }
+        .openbtn {
+            display: none;
+        }
+        .inner-list {
+            display: block!important;
+            height: auto;
+            padding: 10px 0;
+            border: 0;
+            .inner-item:nth-of-type(1){
+                margin-top: 0;
+            }
+        }
+        .inner-item {
+            float: left;
+        }
+    }
+    .togit {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        border: 0;
+        cursor: pointer;
+    }
  }
 </style>
--- myvue/src/pages/home/index.vue
+++ myvue/src/pages/home/index.vue
@@ -1,31 +1,86 @@
 <template>
   <div class="container">
- 
+    <div class="article-wrap">
+      <div class="article-item" v-for="(item, index) in articleList" :key="index">
+        <div class="art-item">
+          <div>{{item.title}}</div>
+          <div>
+            <div>{{item.createTime}}</div>
+            <div>{{item.tags}}</div>
+            <div>{{item.views}}</div>
+            <div>{{item.words}}</div>
+          </div>
+          <div class="imgwrap">
+            <img :src="item.imgSrc" alt="">
+          </div>
+          <div>阅读原文</div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-   
-  },
-  data(){
-    return {
+  export default {
+    props: {
+
+    },
+    data() {
+      return {
+        articleList: [{
+          title: '从 0 到 1 实现 React 系列 —— 5.PureComponent 实现 && HOC 探幽',
+          createTime: '2019-7-25',
+          tags: 'React',
+          imgSrc: 'http://with.muyunyun.cn/9c461a61924ed0fecb6024a256671251.jpg',
+          views: '600',
+          words: '1650'
+        }]
+      }
+    },
+    methods: {
+
+
+    },
+    mounted() {
+
     }
-  },
-  methods:{
-   
-    
-  },
-  mounted(){
   }
-}
 </script>
 <style lang="scss" scoped>
-@import '../../styles/variable.scss';
-.btn {
-    background: pink;
-    font-size: Px(60);
-}
+  @import '../../styles/variable.scss';
+
+  .container {
+    padding-top: 1px;
+    width: 700px;
+    margin: 0 auto;
+  }
+
+  .article-wrap {
+    margin-top: 50px;
+  }
+
+  .article-item {
+    box-sizing: border-box;
+    background: #fff;
+    padding: 2px;
+    border-radius: 2px;
+    box-shadow: 0 0 5px rgba(202, 203, 203, .5);
+    .art-item {
+      padding: 25px;
+      .imgwrap {
+        width: 100%;
+      }
+      .imgwrap img {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
+  @media (max-width: 767px) {
+    .container {
+      width: 100%;
+      margin: 20px auto;
+    }
+  }
 </style>
